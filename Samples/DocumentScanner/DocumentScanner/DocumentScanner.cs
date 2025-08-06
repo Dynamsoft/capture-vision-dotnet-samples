d05385324c16f8453a0e4f5c0bd4ba2cb1fb6988
DMGithubPublisher <github@dynamsoft.com>
2025-08-06 17:50:24 +0800

    update to 3.0.6000

--- Samples/DocumentScanner/DocumentScanner/DocumentScanner.cs
+++ Samples/DocumentScanner/DocumentScanner/DocumentScanner.cs
@@ -18,7 +18,7 @@ namespace DocumentScanner
             string errorMsg;
 
             // Initialize license.
-            // You can request and extend a trial license from https://www.dynamsoft.com/customer/license/trialLicense?product=ddn&utm_source=samples&package=dotnet
+            // You can request and extend a trial license from https://www.dynamsoft.com/customer/license/trialLicense?product=dcv&utm_source=samples&package=dotnet
             // The string 'DLS2eyJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSJ9' here is a free public trial license. Note that network connection is required for this license to work.
             errorCode = LicenseManager.InitLicense("DLS2eyJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSJ9", out errorMsg);
             if (errorCode != (int)EnumErrorCode.EC_OK && errorCode != (int)EnumErrorCode.EC_LICENSE_WARNING)
